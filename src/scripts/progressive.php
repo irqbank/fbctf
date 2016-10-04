f5258b5170a28ad6e499a8833ef23f798a6a5efa
irqbank <irqbank@none.net>
2016-10-04 08:53:33 +0200

    Fix progressive scoreboard, require autoload.php did not work from the shell

--- src/scripts/progressive.php
+++ src/scripts/progressive.php
@@ -1,6 +1,10 @@
 <?hh
 
-require_once ('/var/www/fbctf/vendor/autoload.php');
+require_once('/var/www/fbctf/src/Db.php');
+require_once('/var/www/fbctf/src/Utils.php');
+require_once('/var/www/fbctf/src/models/Model.php');
+require_once('/var/www/fbctf/src/models/Configuration.php');
+require_once('/var/www/fbctf/src/models/Progressive.php');
 
 while (\HH\Asio\join(Progressive::genGameStatus())) {
   \HH\Asio\join(Progressive::genTake());
